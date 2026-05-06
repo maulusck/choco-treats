a7d8c5b9a70412703bc0118ec91365c5cc60e915
Maulusck <64360319+maulusck@users.noreply.github.com>
2026-05-06 16:15:56 +0000

    one more

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-exec sh -c 'trap "exit" INT; while :;do l=/var/log/apache2/error.log;[ "$1" = -a ]&&l="$l /var/log/apache2/access.log"; podman exec -lit sh -c "tail -f $l"; sleep 1; clear;done' sh "$1"
+exec sh -c 'while :;do l=/var/log/apache2/error.log;[ "$1" = -a ]&&l="$l /var/log/apache2/access.log";podman exec -lit sh -c "tail -f $l";sleep 1;clear;done' sh "$1"
